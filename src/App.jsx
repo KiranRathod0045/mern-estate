fccbb6670c34632125c430b078ccaeaac06e9b26
DESKTOP-QC6C26E\Kiran Rathod <kiran.r@cheriseglobl.com>
2025-05-12 12:34:51 +0530

    route created

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
 import React from "react";
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
 
 export default function App() {
-  return <div className="text-red-500">App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/profile" element={<Profile />}></Route>
+        <Route path="/sign-in" element={<SignIn />}></Route>
+        <Route path="/sign-out" element={<SignUp />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
