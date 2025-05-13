6f122a234a9398ea64443df79151f496e7e192f7
DESKTOP-QC6C26E\Kiran Rathod <kiran.r@cheriseglobl.com>
2025-05-13 10:15:41 +0530

    signup api created

--- api/index.js
+++ api/index.js
@@ -1,6 +1,23 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+import userRouter from '../api/routes/user.route.js'
+import authRouter from '../api/routes/auth.route.js'
+dotenv.config()
+
 const app = express();
 
-app.listen(3000,()=>{
+app.use(express.json())
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('DB Connected')
+}).catch((err) => {
+    console.log('err', err)
+})
+
+app.listen(3000, () => {
     console.log(`Server is running on port 3000!`)
 })
+
+app.use('/api/user',userRouter);
+app.use('/api/auth',authRouter);
